ba69255187cc95c219acb291b72754b801fd22b1
Mauro de Pascale <mauro.depascale@hotmail.com>
2024-01-13 12:35:44 +0100

    Minimalistic support for packaging and installation added

---
+++ packaging/doPackage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -f 
+VER=$(git rev-parse --short HEAD)
+if [ $VER == "undefined" ]; then
+  VER=$(git name-rev --name-only --tags HEAD)
+fi
+sed -i -e "s/__VERSION__/$VER/g" extract.sh
+
+tar cvf package.tar -C ../files social-backgrounds@emmedige
+cat extract.sh package.tar > social-backgrounds-$VER.sh
+chmod ug+x  social-backgrounds-$VER.sh
