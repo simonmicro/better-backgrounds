fdba9c3bae1e022c879a9fd6fc38f63defae665e
Mauro de Pascale <mauro.depascale@hotmail.com>
2024-01-21 16:35:55 +0100

    Small fixes for packaging

--- packaging/doPackage.sh
+++ packaging/doPackage.sh
@@ -8,3 +8,4 @@ sed -i -e "s/__VERSION__/$VER/g" extract.sh
 tar cvf package.tar -C ../files social-backgrounds@emmedige
 cat extract.sh package.tar > social-backgrounds-$VER.sh
 chmod ug+x  social-backgrounds-$VER.sh
+echo "RELEASE_TAG=$VER" >> $GITHUB_ENV 
